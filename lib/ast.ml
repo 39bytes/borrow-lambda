d38ea2dd84717d57d64060799fa20950c24ae223
39bytes <jeff@39bytes.dev>
2025-04-02 23:31:57 -0400

    add types

--- lib/ast.ml
+++ lib/ast.ml
@@ -6,8 +6,8 @@ type tm =
   | BorrowMut of tm
   | IfElse of tm * tm * tm
   | LetIn of string * tm * tm
-  | LetMutIn of string * tm * tm
   | Assign of tm * tm
+  | DerefAssign of tm * tm
   | Zero
   | Succ of tm
   | Pred of tm
@@ -15,13 +15,29 @@ type tm =
   | False
   | IsZero of tm
   | Unit
+  | NatVecMake of tm list
+  | NatVecGet of tm * tm
+  | NatVecGetMut of tm * tm
+  | NatVecPush of tm * tm
+  | NatVecPop of tm
 
 type lifetime = Scope of int
+type ref_mod = Mut | Shr
 
 type tp =
   | Nat
   | Bool
   | Unit
   | Arrow of tp * tp
-  | Ref of lifetime * tp
-  | RefMut of lifetime * tp
+  | Ref of lifetime * tp * ref_mod
+
+let copy : tp -> bool = function
+  | Nat | Bool | Unit | Ref (_, _, Shr) -> true
+  | _ -> false
+
+module StringSet = Set.Make (String)
+
+type gamma = (string * tp) list
+type delta = StringSet.t
+type fn_ctx = StringSet.t
+type borrow_ctx = (string * ref_mod) list
