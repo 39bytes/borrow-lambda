d14fa8bef7abce4d4c63f6c5cfbaf08fd01f3c09
39bytes <jeff@39bytes.dev>
2025-04-07 20:05:19 -0400

    uncomment parser

--- lib/parser.ml
+++ lib/parser.ml
@@ -16,50 +16,56 @@ let ident =
 
 let space = skip_while (function ' ' | '\t' | '\n' -> true | _ -> false)
 let syntax s = string s *> space
-let _true = string "true" *> return True
-let _false = string "true" *> return False
-let _unit = string "unit" *> return Unit
-let _zero = char '0' *> return Zero
-let mk_lam x t = Lam (x, t)
-let mk_app t1 t2 = App (t1, t2)
-let mk_borrow x = Borrow x
-let mk_borrow_mut x = BorrowMut x
-let mk_if t1 t2 t3 = IfElse (t1, t2, t3)
-let mk_let x t1 t2 = LetIn (x, t1, t2)
-let mk_assign x t = Assign (x, t)
-let mk_deref_assign x t = DerefAssign (x, t)
+let mk_lam x t = NLam (x, t)
+let mk_app t1 t2 = NApp (t1, t2)
+let mk_borrow x = NBorrow x
+let mk_borrow_mut x = NBorrowMut x
+let mk_if t1 t2 t3 = NIfElse (t1, t2, t3)
+let mk_let x t1 t2 = NLetIn (x, t1, t2)
+let mk_assign x t = NAssign (x, t)
+let mk_deref_assign x t = NDerefAssign (x, t)
 
-(* let term : tm t = *)
-(*   fix (fun term -> *)
-(*       let var = ident in *)
-(*       let lambda = *)
-(*         mk_lam *)
-(*         <$> (string "\\" <|> string "λ") *> ident *)
-(*         <* char '.' <* space <*> term *)
-(*       in *)
-(*       let app = mk_app <$> term <* space <*> term in *)
-(*       let borrow = mk_borrow <$> char '&' *> term in *)
-(*       let borrow_mut = mk_borrow_mut <$> string "&mut " *> term in *)
-(*       let if_then_else = *)
-(*         mk_if *)
-(*         <$> syntax "if" *> term *)
-(*         <* space *)
-(*         <*> syntax "then" *> term *)
-(*         <* space *)
-(*         <*> syntax "else" *> term *)
-(*         <* space *)
-(*       in *)
-(*       let let_in = *)
-(*         mk_let *)
-(*         <$> syntax "let" *> ident *)
-(*         <* space *)
-(*         <*> syntax "=" *> term *)
-(*         <* space *)
-(*         <*> syntax "in" *> term *)
-(*         <* space *)
-(*       in *)
-(*       let assign = mk_assign <$> ident <* space <*> syntax ":=" *> term in *)
-(*       let deref_assign = *)
-(*         mk_deref_assign <$> char '*' *> ident <* space <*> syntax ":=" *> term *)
-(*       in *)
-(*       raise NotImplemented) *)
+let term : named_tm t =
+  fix (fun term ->
+      let var = ident in
+      let lambda =
+        mk_lam
+        <$> (string "\\" <|> string "λ") *> ident
+        <* char '.' <* space <*> term
+      in
+      let app = mk_app <$> term <* space <*> term in
+      let borrow = mk_borrow <$> char '&' *> term in
+      let borrow_mut = mk_borrow_mut <$> string "&mut " *> term in
+      let if_then_else =
+        mk_if
+        <$> syntax "if" *> term
+        <* space
+        <*> syntax "then" *> term
+        <* space
+        <*> syntax "else" *> term
+        <* space
+      in
+      let let_in =
+        mk_let
+        <$> syntax "let" *> ident
+        <* space
+        <*> syntax "=" *> term
+        <* space
+        <*> syntax "in" *> term
+        <* space
+      in
+      let assign = mk_assign <$> ident <* space <*> syntax ":=" *> term in
+      let deref_assign =
+        mk_deref_assign <$> char '*' *> ident <* space <*> syntax ":=" *> term
+      in
+      let _zero = char '0' *> return NZero in
+      let succ = string "Succ" *> space *> term in
+      let pred = string "Pred " *> space *> term in
+      let pred = string "Pred " *> space *> term in
+      let _true = string "true" *> return NTrue in
+      let _false = string "true" *> return NFalse in
+      let _unit = string "unit" *> return NUnit in
+
+      let parenthesized = char '(' *> space *> term <* space <* char ')' in
+
+      raise NotImplemented)
